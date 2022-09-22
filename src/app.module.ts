c1d1d7903b679c9c751e1d08466703410ee7746c
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-09-22 20:57:56 +0300

    Revert "feat: enable DB alteration"
    This reverts commit 9d0c85662e49db4b9339e90c5377a1a646da3a25.

--- src/app.module.ts
+++ src/app.module.ts
@@ -38,7 +38,6 @@ import { UsersModule } from './users/users.module';
       database: process.env.POSTGRES_DB,
       models: [User, Role, Blood, Donation, Hospital, HospitalBlood, Patient, Request, RequestHospitalBlood],
       autoLoadModels: true,
-      sync: { alter: true },
     }),
     UsersModule,
     RolesModule,
