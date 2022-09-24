c0c2397822d05fbe76c7b13a4da66be0a85a05f6
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-09-24 13:54:18 +0300

    cleanup: imports & useless code

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { BloodService } from 'src/blood/blood.service';
-import { createRoleDto } from 'src/roles/dto/create-role-dto';
-import { IRoleName, Role } from 'src/roles/roles.model';
+import { Role } from 'src/roles/roles.model';
 import { RolesService } from 'src/roles/roles.service';
 import { createAnyDto } from './dto/create-any-dto';
 import { createUserDto } from './dto/create-user-dto';
@@ -40,11 +39,8 @@ export class UsersService {
     const user = await this.userRepository.create(dto);
     const role = await this.roleService.getRoleByValue(dto.role);
     await user.$set('role', role.id);
-    // return user with role, because $set doesn't mutate initial data
-    user.role = role;
     const blood = await this.bloodService.getBloodByValue(dto.blood);
     await user.$set('blood', blood.id);
-    user.blood = blood;
     return user;
   }
 
