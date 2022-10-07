b290cc2c61e78baebfc3533add7d4dd36324c71d
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-07 20:55:35 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 }
 
